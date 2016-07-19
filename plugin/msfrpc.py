fbc2e0a80e26d3e0dd8007b3717937e4b3e5aeaa
Sliim <sliim@mailoo.org>
2016-07-19 04:08:02 +0200

    msfrpc: Improved logging

--- plugin/msfrpc.py
+++ plugin/msfrpc.py
@@ -63,25 +63,39 @@ class Msfrpc:
 
 class CscanMsf:
     """ msfrpc plugin for cscan """
-    def __init__(self):
-        self.logfile = None
+    def __init__(self, logfile=False, quiet=False):
+        self.logfile = logfile
         self.cid = None
+        self.quiet = quiet
         try:
             self.client = Msfrpc({"host": os.environ.get("CS_MSF_HOST"), "port": os.environ.get("CS_MSF_PORT")})
             self.client.login(os.environ.get("CS_MSF_USER"), os.environ.get("CS_MSF_PASS"))
-            print "Logged in to msfrpc server. Token: %s" % self.client.token
+            self.log("Logged in to msfrpc server. Token: %s" % self.client.token, True)
         except:
-            print "Cannot connect to server.."
+            self.log("ERROR: Cannot connect to server..", True)
 
     def check_auth(self):
         if not self.client or not self.client.authenticated:
-            print "You are not authenticated.."
+            self.log("ERROR: You are not authenticated..", True)
             return False
         return True
+
+    def log(self, msg, critical=False):
+        if self.logfile:
+            logfile = open(self.logfile, "a")
+            logfile.write("%s\n" % msg)
+            logfile.close()
+        if not self.quiet or critical:
+            print msg
 
     def rpc_call(self, meth, opts, key=""):
         if self.check_auth():
             res = self.client.call(meth, opts)
+
+            if "error" in res:
+                self.log("ERROR: %s %s" % (res["error_code"], res["error_message"]), True)
+                self.log("%s: %s\n%s" % (res["error_class"], res["error_string"], res["error_backtrace"]))
+                return res
             return res if not key else res.get(key)
 
     def create_console(self):
@@ -90,78 +104,65 @@ class CscanMsf:
         return self.cid
 
     def destroy_console(self):
-        print "Destroy console ID %s.. %s" % (self.cid, self.rpc_call("console.destroy",
-                                                                      [self.cid], "result"))
-
-    def set_logfile(self, f):
-        self.logfile = open(f, "a+")
+        self.log("Destroy console ID %s.. %s" % (self.cid, self.rpc_call("console.destroy",
+                                                                         [self.cid], "result")), True)
 
     def create_ws(self, ws, switch=False):
-        print "Create %s workspace.. %s" % (ws, self.rpc_call("db.add_workspace", [ws], "result"))
+        self.log("Create %s workspace.. %s" % (ws, self.rpc_call("db.add_workspace", [ws], "result")), True)
         if switch:
             self.set_ws(ws)
         return ws
 
     def set_ws(self, ws):
-        print "Switch to new workspace.. %s" % self.rpc_call("db.set_workspace", [ws], "result")
+        self.log("Switch to %s workspace.. %s" % (ws, self.rpc_call("db.set_workspace", [ws], "result")), True)
 
     def destroy_ws(self, ws):
-        print "Delete %s workspace.. %s" % (ws, self.rpc_call("db.del_workspace", [ws], "result"))
+        self.log("Delete %s workspace.. %s" % (ws, self.rpc_call("db.del_workspace", [ws], "result")), True)
 
     def import_xml_data(self, ws, xml):
         content = open(xml, "r").read()
-        print "Importing data from %s.. %s" % (xml, self.rpc_call("db.import_data", [{"workspace": ws,
-                                                                                      "data": content}], "result"))
+        self.log("Importing data from %s.. %s" % (xml, self.rpc_call("db.import_data", [{"workspace": ws,
+                                                                                         "data": content}], "result")), True)
 
     def export_current_ws(self, out):
-        print "Exporting workspace.."
+        self.log("Exporting workspace..", True)
         self.rpc_call("console.write", [self.cid, "db_export %s\r\n" % out])
     
         while True:
             time.sleep(5)
             res = self.rpc_call("console.read", [self.cid])
             if res.get("data"):
-                print "%s %s" % (res.get("prompt"), res.get("data"))
+                self.log("%s %s" % (res.get("prompt"), res.get("data")))
             if "Finished export" in res.get("data"):
                 return True
             
     def wait_for_jobs(self):
         while True:
             job_list = self.rpc_call("job.list", [])
-            print "Current jobs: %s (Total: %d)" % (",".join(job_list), len(job_list))
+            self.log("Current jobs: %s (Total: %d)" % (",".join(job_list), len(job_list)), True)
             if len(job_list) > 0:
                 for j in job_list:
                     jinfo = self.rpc_call("job.info", [j])
-                    print "%s - %s" % (jinfo.get("jid"), jinfo.get("name"))
+                    self.log("%s - %s" % (jinfo.get("jid"), jinfo.get("name")), True)
             else:
                 return True
             time.sleep(10)
 
-    def run_commands(self, commands, quiet=False):
-            print "Run command: %s" % "\n".join(commands)
-            self.rpc_call("console.write", [self.cid, "\n".join(commands)])
-            self.rpc_call("console.write", [self.cid, "set PROMPT commands_deployed\r\n"])
+    def run_commands(self, commands):
+        self.log("Run command: %s" % "\n".join(commands), True)
+        self.rpc_call("console.write", [self.cid, "\n".join(commands)])
+        self.rpc_call("console.write", [self.cid, "set PROMPT commands_deployed\r\n"])
 
-            while True:
-                time.sleep(2)
-                res = self.rpc_call("console.read", [self.cid])
-                if self.logfile:
-                    self.logfile.write(str(res) + "\n")
-                if not quiet and res.get("data"):
-                    print "%s %s" % (res.get("prompt"), res.get("data")) 
-                if "commands_deployed" in res["prompt"] and not res["busy"]:
-                    self.rpc_call("console.write", [self.cid, "set PROMPT cscan\r\n"])
-                    break
-
-    def clean(self):
-        if self.logfile:
-            self.logfile.close()
-        self.wait_for_jobs()
-        self.rpc_call("console.write", [self.cid, "set PROMPT msf\r\n"])
-        time.sleep(1)
+        while True:
+            time.sleep(2)
+            res = self.rpc_call("console.read", [self.cid])
+            if res.get("data"):
+                self.log("%s %s" % (res.get("prompt"), res.get("data")), True)
+            if "commands_deployed" in res["prompt"] and not res["busy"]:
+                self.rpc_call("console.write", [self.cid, "set PROMPT cscan\r\n"])
+                break
 
 def main():
-    cscan = CscanMsf()
     parser = argparse.ArgumentParser(description="msfrpc cscan plugin, for automated security testing")
     parser.add_argument("-o","--output", help="Output file", required=False)
     parser.add_argument("-l","--log", help="Log file", required=False)
@@ -173,10 +174,11 @@ def main():
     parser.add_argument("-T","--disable-tmp-ws", help="Do not create temp workspace and use current", required=False)
     parser.add_argument("-q","--quiet", help="Quiet mode, set -l options to have log in a file", required=False, action="store_true")
     args = parser.parse_args()    
-    
-    current_ws = cscan.rpc_call("db.current_workspace", [], "workspace")
-    tmp_ws = None
 
+    cscan = CscanMsf(args.log if args.log else False,
+                     args.quiet if args.quiet else False)
+    tmp_ws = None
+    current_ws = cscan.rpc_call("db.current_workspace", [], "workspace")
     print "Current workspace: " + current_ws
     if not args.disable_tmp_ws and os.environ.get("CS_MSF_TMP_WS") == "enabled":
         tmp_ws = "cscan_" + "".join(random.sample(string.lowercase,6))
@@ -196,7 +198,6 @@ def main():
     if args.output:
         print "Output: " + args.output
     if args.log:
-        cscan.set_logfile(args.log)
         print "Log file: " + args.log
 
     commands = []
@@ -215,8 +216,10 @@ def main():
         commands.append("resource " + args.resource)
 
     commands.append("\r\n")
-    cscan.run_commands(commands, args.quiet if args.quiet else False)
-    cscan.clean()
+    cscan.run_commands(commands)
+    cscan.wait_for_jobs()
+    cscan.rpc_call("console.write", [console_id, "set PROMPT msf\r\n"])
+    time.sleep(1)
 
     if os.environ.get("CS_MSF_EXPORT") == "enabled" and args.output:
         cscan.export_current_ws(args.output)
