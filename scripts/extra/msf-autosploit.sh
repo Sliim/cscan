259340487d70e6cc7ed4ab8fcbc1661b7563f76b
Sliim <sliim@mailoo.org>
2016-09-06 23:18:40 +0200

    Fixed msf-autosploit.sh resource path

--- scripts/extra/msf-autosploit.sh
+++ scripts/extra/msf-autosploit.sh
@@ -11,7 +11,7 @@ echo "Run msfrpc plugin.."
 ./plugin/msfrpc.py --output $(realpath $2$NAME.xml) \
 		           --log $(realpath $3$NAME.log) \
 		           --xml $xml \
-		           --resource scripts/resources/autosploit.rc \
+		           --resource $(realpath scripts/resources/autosploit.rc) \
 		           --options THREADS=100:TARGET_PLATFORM=aix,android,bsdi,dialup,firefox,freebsd,hpux,irix,linux,mainframe,multi,netware,solaris,unix:BLACKLIST=freebsd/samba/trans2open,linux/samba/trans2open,osx/samba/trans2open,solaris/samba/trans2open
 
 cp $2$NAME.xml $xml
